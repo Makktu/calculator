afdf4d6fe630e19dd27172d3611a48e8a9b11e94
Makktu <johnmcna@gmail.com>
2021-07-25 01:35:49 +0100

    bit better

--- script.js
+++ script.js
@@ -1,89 +1,77 @@
 "use strict";
 
-// * ADD, SUBTRACT, MULTIPLY, DIVIDE
-
-const add = (a, b) => {
-    a + b;
-};
-const subtract = (a, b) => a - b;
-const multiply = (a, b) => a * b;
-const divide = (a, b) => a / b;
-
-// function opNumbers(a, b, op) {
-//     if (op === "a" || op === "A") {
-//         console.log(addNumbers(a, b));
-//     } else if (op === "s" || op === "S") {
-//         console.log(subtractNumbers(a, b));
-//     } else if (op === "m" || op === "M") {
-//         console.log(multiplyNumbers(a, b));
-//     } else if (op === "d" || op === "D") {
-//         console.log(divideNumbers(a, b));
-//     }
-// }
-
-// let a = Number(prompt("Enter 1st number"));
-// let b = Number(prompt("Enter 2nd number"));
-// let op = prompt("Add, Subtract, Multiply, Divide? (A, S, M, D)");
-
-// opNumbers(a, b, op);
-
-//
-// console.log(subtractNumbers(a, b));
-//
-// console.log(divideNumbers(a, b));
-
-// attach event listeners to all buttons
-
 function buttonClicked() {
+    // get value of button
+    currentBtnPress = this.innerHTML;
+    // if numerical, add number to display number
     if (this.classList.contains("num")) {
-        if (!number1) {
-            number1 = this.innerHTML;
-            currentNumber += parseInt(number1);
-        } else {
-            number2 = this.innerHTML;
-            currentNumber += parseInt(number2);
-            console.log("2", number2, currentNumber);
+        if (displayNum.length < 8) {
+            displayNum += currentBtnPress;
+            displayArea.textContent = displayNum;
         }
-        displayArea.textContent = currentNumber;
-    } else if (this.innerHTML === "C") {
-        displayArea.textContent = "";
+    }
+
+    if (currentBtnPress === "C") {
+        displayArea.textContent = "0";
         sumArea.textContent = "";
-        currentNumber = "";
-        sumInProgress = "";
-    } else if (this.innerHTML === "=") {
-        console.log("EQUALS!");
-    } else if (this.classList.contains("operator")) {
-        if (this.innerHTML === "+") {
-            if (number1) {
-                sumArea.textContent = number1 + number2;
-            }
+        displayNum = "";
+        number1 = 0;
+        number2 = 0;
+    }
+
+    if (currentBtnPress === "+") {
+        // lastOp = "add";
+
+        if (!number1) {
+            number1 = parseFloat(displayNum);
+        } else {
+            number1 += parseFloat(displayNum);
         }
-    } else {
-        calcSum(sumInProgress);
+        sumArea.textContent = number1 + "+";
+        displayArea.textContent = "0";
+        displayNum = "";
+    }
+
+    if (currentBtnPress === "-") {
+        // lastOp = "minus";
+        if (!number1) {
+            number1 = parseFloat(displayNum);
+        } else {
+            number1 -= parseFloat(displayNum);
+        }
+        sumArea.textContent = number1 + "-";
+        displayArea.textContent = "0";
+        displayNum = "";
+    }
+
+    if (currentBtnPress === "=" && number1) {
+        // lastOp = "equals";
+        if (lastOp === "add") number2 = number1 + parseFloat(displayNum);
+        if (lastOp === "minus") number2 = number1 - parseFloat(displayNum);
+        if (lastOp === "mult") number2 = number1 * parseFloat(displayNum);
+        if (lastOp === "divide") number2 = number1 / parseFloat(displayNum);
+
+        sumArea.textContent = "";
+        displayArea.textContent = number2;
+        displayNum = number2;
     }
 }
 
-function calcSum(sum) {
-    // parse the sum in progress!
-}
-
 const displayArea = document.querySelector(".display");
+
+displayArea.textContent = "0";
 
 const sumArea = document.querySelector(".sum-display");
 
 const allButtons = document.querySelectorAll(".btn");
 
-let currentNumber;
-let sumInProgress = "";
-let goFunc;
+// * define main variables
+
+let rollingSum = 0;
 let currentBtnPress;
-let lastSum = "";
-
-let action = null;
-
-let number1 = null;
-let number2 = null;
-
-let currentOperator = "";
+let number1;
+let number2;
+let displayNum = "";
+let lastOp;
 
 allButtons.forEach((button) => button.addEventListener("click", buttonClicked));
