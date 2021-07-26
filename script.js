18084d905fd2eb9bbba4f5d4be45c5ad7e82deea
Makktu <johnmcna@gmail.com>
2021-07-26 19:54:30 +0100

    Tangled

--- script.js
+++ script.js
@@ -1,78 +1,117 @@
 "use strict";
 
-function buttonClicked() {
-    // get value of button
-    currentBtnPress = this.innerHTML;
-    // if numerical, add number to display number
-    if (this.classList.contains("num")) {
-        if (displayNum.length < 9) {
-            displayNum += currentBtnPress;
-            displayArea.textContent = displayNum;
-        }
+const numberClicked = () => {
+    // if 'displayNum' is less than the screen size, we add the entry to the number visible; if not less, nothing happens.
+    if (number1.length < 8) {
+        number1 += currentBtnPress;
+        displayArea.textContent = number1;
     }
+};
 
-    if (currentBtnPress === "C") {
-        displayArea.textContent = "0";
-        sumArea.textContent = "";
-        displayNum = "";
-        number1 = 0;
-        number2 = 0;
+const deleteClicked = () => {
+    if (number1.length > 0) {
+        number1 = number1.substring(0, number1.length - 1);
+        displayArea.textContent = number1;
     }
+};
 
-    if (currentBtnPress === "+") {
+const addClicked = () => {
+    if (lastOp !== "equals") {
+        sumString += parseFloat(number1) + " + ";
+    } else if (lastOp === "equals") {
+        sumString = parseFloat(number1) + " + ";
+        printSumstring(sumString);
+        displayArea.textContent = number1;
         lastOp = "add";
+        return;
+    }
+    number2
+        ? (number2 = parseFloat(number2) + parseFloat(number1))
+        : (number2 = parseFloat(number1));
 
-        if (!number1) {
-            number1 = parseFloat(displayNum);
-        } else {
-            number1 += parseFloat(displayNum);
-        }
+    displayArea.textContent = parseFloat(number2);
 
-        sumArea.textContent = number2 + "+";
+    printSumstring(sumString);
+    lastOp = "add";
+    number1 = "";
+};
 
-        displayArea.textContent = "0";
-        displayNum = "";
+const subtractClicked = () => {
+    //stuff
+};
+
+const multiplyClicked = () => {
+    // stuff
+};
+
+const divideClicked = () => {
+    //stuff
+};
+
+const equalsClicked = () => {
+    if (number2) {
+        sumString += parseFloat(number1) + " = ";
+        number2 = parseFloat(number2) + parseFloat(number1);
+        displayArea.textContent = parseFloat(number2);
+
+        printSumstring(sumString);
+        lastOp = "equals";
+        number1 = number2;
+    }
+};
+
+const c_clicked = () => {
+    sumArea.textContent = "";
+    number1 = "";
+    displayArea.textContent = "0";
+    number2 = "";
+    lastOp = "";
+    sumString = "";
+    // number2 = 0;
+    // lastOp = "";
+};
+
+const printSumstring = (sumString) => {
+    if (sumString.length > 22) {
+        sumArea.style.cssText = "font-size: .75rem;";
     }
 
-    if (currentBtnPress === "-") {
-        // lastOp = "minus";
-        if (!number1) {
-            number1 = parseFloat(displayNum);
-        } else {
-            number1 -= parseFloat(displayNum);
-        }
-        sumArea.textContent = number1 + "-";
-        displayArea.textContent = "0";
-        displayNum = "";
+    if (sumString.length > 35) {
+        sumString =
+            "..." +
+            sumString.substring(sumString.length - 35, sumString.length);
     }
+    sumArea.textContent = sumString;
+};
 
-    if (currentBtnPress === "=" && number1) {
-        if (lastOp === "add") number2 = number1 + parseFloat(displayNum);
-        if (lastOp === "minus") number2 = number1 - parseFloat(displayNum);
-        if (lastOp === "mult") number2 = number1 * parseFloat(displayNum);
-        if (lastOp === "divide") number2 = number1 / parseFloat(displayNum);
-
-        sumArea.textContent = "";
-        displayArea.textContent = number2;
-        displayNum = number2;
-    }
+function buttonClicked() {
+    currentBtnPress = this.innerHTML;
+    // console.log(currentBtnPress, numberSum, displayNum);
+    if (this.classList.contains("num")) numberClicked();
+    if (currentBtnPress === "DEL") deleteClicked();
+    if (currentBtnPress === "C") c_clicked();
+    if (currentBtnPress === "+") addClicked();
+    if (currentBtnPress === "-") subtractClicked();
+    if (currentBtnPress === "*") multiplyClicked();
+    if (this.classList.contains("divide")) divideClicked();
+    if (currentBtnPress === "=" && number1) equalsClicked();
 }
 
+// * define the main display area – where the user entry will appear, and give it a default display of '0'
 const displayArea = document.querySelector(".display");
-
 displayArea.textContent = "0";
 
+// * define the subsidiary display where the sum-in-progress details will appear
 const sumArea = document.querySelector(".sum-display");
 
+// * assign event listener to all buttons
 const allButtons = document.querySelectorAll(".btn");
+allButtons.forEach((button) => button.addEventListener("click", buttonClicked));
 
 // * define main variables
-
-let rollingSum = 0;
 let currentBtnPress;
-let number1;
-let number2;
-let displayNum = "";
-let lastOp;
-
-allButtons.forEach((button) => button.addEventListener("click", buttonClicked));
+let number1 = ""; // this will be the next number – the live number
+let number2 = "";
+// let displayNum = "";
+let lastOp = "";
+let sumString = "";
