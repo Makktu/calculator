e3de73fa1ca657a579532935b1db25c26ea9b864
Makktu <johnmcna@gmail.com>
2022-06-16 17:30:17 +0100

    added offline support

--- service-worker.js
+++ service-worker.js
@@ -1,8 +1,25 @@
-self.addEventListener("install", function () {
-    console.log("Install!");
+const cacheName = "calc-1";
+const staticAssets = [
+    "./",
+    "./index.html",
+    "./script.js",
+    "./node_modules",
+    "./manifest.json",
+    "./package-lock.json",
+    "./README.md",
+    "./assets",
+    "./service-worker.js",
+];
+
+self.addEventListener("install", async (e) => {
+    console.log("SW installed");
+    const cache = await caches.open(cacheName);
+    await cache.addAll(staticAssets);
+    return self.skipWaiting();
 });
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", (e) => {
     console.log("Activate!");
+    self.clients.claim();
 });
 self.addEventListener("fetch", function (event) {
     console.log("Fetch!", event.request);
